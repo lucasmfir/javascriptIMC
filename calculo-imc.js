c99a649a88266a7d6ba349b13c3cbbe44711c6b3
Lucas Firmo <lucasmfir@gmail.com>
2017-01-03 23:34:13 -0200

    anonymous functions

--- calculo-imc.js
+++ calculo-imc.js
@@ -1,49 +1,41 @@
-//document.getElementsByTagName("tr");  pega todos os elementos a partir da tag
-/*
-var tdPeso1 = document.getElementById("peso-1");
-var tdAltura1 = document.getElementById("altura-1");
+var botao = document.getElementById("calcula-imcs");
+botao.addEventListener("click", function(){
 
-var peso1 = tdPeso1.textContent;
-var altura1 = tdAltura1.textContent;
+	var trPacientes = document.getElementsByClassName("paciente");
 
-var paciente1 = {"peso" : peso1, "altura" : altura1};
-//paciente.peso = paciente["peso"]
+	percorreArray(trPacientes, calculaImc);
 
-var tdPeso2 = document.getElementById("peso-2");
-var tdAltura2 = document.getElementById("altura-2");
+	function calculaImc(trPaciente){
+		var tdNome = trPaciente.getElementsByClassName("info-nome")[0];
+		var tdPeso = trPaciente.getElementsByClassName("info-peso")[0];
+		var tdAltura = trPaciente.getElementsByClassName("info-altura")[0];
 
-var peso2 = tdPeso2.textContent;
-var altura2 = tdAltura2.textContent;
+		var paciente = {"nome" : tdNome.textContent , 
+						"peso": tdPeso.textContent , 
+						"altura" : tdAltura.textContent,
+						"calcImc" : function(){
+							if (this.altura != 0){
+								var imc = this.peso/(this.altura*this.altura);
+								return imc;
+							} else {
+								console.log("altura igual a zero");
+							}
+						}};
 
-var paciente2 = {"peso" : peso2, "altura" : altura2};
+		var imc = paciente.calcImc();
 
-//paciente.peso = paciente["peso"]
-
-
-
-
-var pacientes = [paciente1, paciente2];
-var posicao = 0;
-console.log("aki");
-*/
-var trPacientes = document.getElementsByClassName("paciente");
-
-
-for(var posicao = 0; posicao < trPacientes.length; posicao++){
-	var trPaciente = trPacientes[posicao];
-
-	var tdNome = trPaciente.getElementsByClassName("info-nome")[0];
-	var tdPeso = trPaciente.getElementsByClassName("info-peso")[0];
-	var tdAltura = trPaciente.getElementsByClassName("info-altura")[0];
-
-	var paciente = {"nome" : tdNome.textContent , "peso": tdPeso.textContent , "altura" : tdAltura.textContent};
-
-	if (tdAltura != 0){
-		var imc = paciente.peso/(paciente.altura*paciente.altura);
 		var tdImc = trPaciente.getElementsByClassName("info-imc")[0];
 		tdImc.textContent = imc; 
-	} else {
-		console.log("altura igual a zero");
 	}
 
-}
+})
+
+
+
+
+
+
+botao.addEventListener("click", function(){
+	console.log("calculando IMCS");
+});
+
