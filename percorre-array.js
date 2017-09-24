73c4359c08509c30ddb0e7bcaafed98867e88117
Lucas Firmo <lucasmfir@gmail.com>
2017-09-24 16:48:57 -0300

    final prj

--- percorre-array.js
+++
@@ -1,9 +0,0 @@
-
-function percorreArray(trPacientes, comportamento){
-for(var posicao = 0; posicao < trPacientes.length; posicao++){
-	var trPaciente = trPacientes[posicao];
-
-	comportamento(trPaciente);
-
-	}
-}
